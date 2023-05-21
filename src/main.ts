14563c59642f97ffa8af3c6b00e8c4ce5fb4e8f6
Ahmed Hamdy <hamdimn54@gmail.com>
2023-05-21 13:13:18 +0200

    [Serverless] add support for serverless

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import serverlessExpress from '@vendia/serverless-express';
+import { Callback, Context, Handler } from 'aws-lambda';
 import { AppModule } from './app.module';
+
+let serverHandler: Handler;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.init();
+
+  const expressApp = app.getHttpAdapter().getInstance();
+  return serverlessExpress({ app: expressApp });
 }
-bootstrap();
+
+export const handler = async (
+  event: any,
+  context: Context,
+  callback: Callback,
+) => {
+  serverHandler = serverHandler ?? (await bootstrap());
+  return serverHandler(event, context, callback);
+};
